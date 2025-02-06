ab06e74087d58e1065e3b8c352268c3dcaaa309f
Tran Anh Tuan <trananhtuan.tdc.2223@gmail.com>
2025-02-07 00:04:38 +0700

    add register

--- Prj_Social_Media/routes/web.php
+++ Prj_Social_Media/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('home');
+Route::prefix('v1')->group(function () {
+    // Hiển thị form đăng ký
+    Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [AuthController::class, 'register']);
+    // Hiển thị form đăng nhập
+    Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    // Hiển thị trang chủ
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
 });
