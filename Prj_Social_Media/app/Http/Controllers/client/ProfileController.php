069d7a48658fa68b61acc890e681197da2fef566
dangvanthua <thuandang.021102@gmail.com>
2025-02-09 19:27:56 +0700

    commit code

--- Prj_Social_Media/app/Http/Controllers/client/ProfileController.php
+++ Prj_Social_Media/app/Http/Controllers/client/ProfileController.php
@@ -8,10 +8,8 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller {
     
     public function index() {
-            // Lấy user hiện tại (nếu chưa đăng nhập, bạn có thể xử lý fallback hoặc chuyển hướng)
         $user = auth()->user();
         
-        // Nếu chưa đăng nhập, ví dụ chuyển hướng về trang login hoặc dùng user mẫu (không khuyến khích, chỉ cho ví dụ)
         if (!$user) {
             // Chuyển hướng về trang đăng nhập
             // return redirect()->route('login')->with('error', 'Vui lòng đăng nhập để xem profile.');
